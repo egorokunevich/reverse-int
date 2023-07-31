ba471ed93ffcaa62c666b07d2d64bcb19aa9cb91
egorokunevich <mrcraigre@gmail.com>
2023-07-31 22:06:17 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let num = String(n);
+  let res = '';
+  for (let i = (num.length - 1); i >= 0; i--) {
+    if (isNaN(num[i])) {
+      continue
+    }
+    else {
+      res = res + num[i];
+    }
+  }
+  return(res);
 }
